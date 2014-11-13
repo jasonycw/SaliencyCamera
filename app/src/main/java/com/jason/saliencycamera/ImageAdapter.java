70789e4b4d539500ac5f431b0b1225cdfd467f41
Jason Yu <jasonycw1992@gmail.com>
2014-11-13 22:38:28 +0800

    [ADD] more test image and related UI changes

---
+++ app/src/main/java/com/jason/saliencycamera/ImageAdapter.java
@@ -0,0 +1,70 @@
+package com.jason.saliencycamera;
+
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+/**
+ * Created by Jason on 13/11/2014.
+ */
+public class ImageAdapter extends BaseAdapter {
+    // references to our images
+    public static String[] NO_FLASH_IMAGE_NAMES = {
+            "toy.jpg","3dprinting.jpg","totoro11.jpg","totoro22.jpg","winnie.jpg","lena2_.png"
+    };
+    public static final String[] FLASH_IMAGE_NAMES = {
+            "toy_lighted.jpg","3dprinting_lighted.jpg","totoro11_lighted.jpg","totoro22_lighted.jpg","winnie_lighted.jpg","lena1_.png"
+    };
+
+    private Context mContext;
+    private GridView mGridView;
+
+    public ImageAdapter(Context c, GridView g) {
+        mContext = c;
+        mGridView = g;
+    }
+
+    public int getCount() {
+        return NO_FLASH_IMAGE_NAMES.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {  // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+//            imageView.setLayoutParams(new GridView.LayoutParams(GridView.LayoutParams.MATCH_PARENT, (int) (mGridView.getHeight()/2.5)));
+            imageView.setLayoutParams(new GridView.LayoutParams(GridView.LayoutParams.MATCH_PARENT, GridView.LayoutParams.MATCH_PARENT));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+//        imageView.setImageResource(NO_FLASH_IMAGE_NAMES[position]);
+        try {
+            imageView.setImageBitmap(BitmapFactory.decodeStream(mContext.getAssets().open(NO_FLASH_IMAGE_NAMES[position])));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return imageView;
+    }
+
+
+}
--- app/src/main/java/com/jason/saliencycamera/TestImageActivity.java
+++ app/src/main/java/com/jason/saliencycamera/TestImageActivity.java
@@ -2,14 +2,13 @@ package com.jason.saliencycamera;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
-
-import java.io.IOException;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 
 public class TestImageActivity extends Activity {
@@ -19,39 +18,16 @@ public class TestImageActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_image);
 
-        ImageView lenaImage = (ImageView)findViewById(R.id.lenaImage);
-        try {
-            lenaImage.setImageBitmap(BitmapFactory.decodeStream(getApplicationContext().getAssets().open("lena1_.png")));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        lenaImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Pass data to next activity
+        GridView gridview = (GridView) findViewById(R.id.gridView);
+        gridview.setAdapter(new ImageAdapter(this,gridview));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+//                Toast.makeText(HelloGridView.this, "" + position, Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(TestImageActivity.this, ImageViewActivity.class);
                 Bundle bundle = new Bundle();
-                bundle.putString("drawable1", "lena2_.png");
-                bundle.putString("drawable2", "lena1_.png");
-                bundle.putLong("timeDifference", 0);
-                intent.putExtras(bundle);
-                startActivity(intent);
-            }
-        });
-        ImageView toyImage = (ImageView)findViewById(R.id.toyImage);
-        try {
-            toyImage.setImageBitmap(BitmapFactory.decodeStream(getApplicationContext().getAssets().open("toy.jpg")));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        toyImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Pass data to next activity
-                Intent intent = new Intent(TestImageActivity.this, ImageViewActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putString("drawable1", "toy.jpg");
-                bundle.putString("drawable2", "toy_lighted.jpg");
+                bundle.putString("drawable1", ImageAdapter.NO_FLASH_IMAGE_NAMES[position]);
+                bundle.putString("drawable2", ImageAdapter.FLASH_IMAGE_NAMES[position]);
                 bundle.putLong("timeDifference", 0);
                 intent.putExtras(bundle);
                 startActivity(intent);
