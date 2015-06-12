c4d3a07b9712bc035a884313314d776ccd59e941
Jason Yu <jasonycw1992@gmail.com>
2015-06-12 10:57:33 +0800

    [FIXING] js not loading when page load

--- ProjectPage/js/loadTestCases.js
+++ ProjectPage/js/loadTestCases.js
@@ -28,7 +28,35 @@ $( window ).load(function(){
 	}
 
 	$(".lightImage").fadeTo(0,0.5);
-	console.log("Test cases are loaded");
+		console.log("Test cases are loaded");
+
+		$(".imagePair").mouseenter(function(){
+		$(".lightImage").fadeTo(0,1);
+		$(".inputImagesHeader").text("Flash Images");
+		$(".lshiifImagesHeader").text("Flash LSHIIF");
+		showLightImage = true;
+	});
+
+	$(".imagePair").mouseleave(function(){
+		$(".lightImage").fadeTo(0,0.5);
+		$(".inputImagesHeader").text("Image Pairs");
+		$(".lshiifImagesHeader").text("LSHIIF");
+	});
+
+	$(".imagePair").click(function() {
+		if(showLightImage){
+			$(".lightImage").fadeTo(0,0);
+			$(".inputImagesHeader").text("No flash Images");
+			$(".lshiifImagesHeader").text("No flash LSHIIF");
+			showLightImage = false;
+		}
+		else{
+			$(".lightImage").fadeTo(0,1);
+			$(".inputImagesHeader").text("Flash Images");
+			$(".lshiifImagesHeader").text("Flash LSHIIF");
+			showLightImage = true;
+		}
+	});
 });
 
 // $( document ).ready(function(){
